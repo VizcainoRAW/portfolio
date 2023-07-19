46c86a3603ebf0f67a215e3add8a24a46291a473
Ivan Vizcaino <ivizcain4@cuc.edu.co>
2023-07-19 17:39:40 -0500

    added experience css style

--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,36 @@ window.addEventListener('scroll', function() {
       navbar.classList.remove('scrolled');
     }
   });
+
+  document.addEventListener('DOMContentLoaded', function() {
+    openJob(event, 'firtsJob');
+  });
+
+  function openJob(evt, jobId) {
+    var i, tabcontent, tablinks;
+    var topIndicator=0;
+  
+    tabcontent = document.getElementsByClassName("job");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+  
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].className = tablinks[i].className.replace(" active", "");
+    }
+  
+    document.getElementById(jobId).style.display = "block";
+    evt.currentTarget.className += " active";
+  
+    for (i = 0; i < tablinks.length; i++) {
+      if (tabcontent[i].id == jobId) {
+        break;
+      }
+      topIndicator++;
+    }
+  
+    var indicator = document.getElementsByClassName("MuiTabs-indicator");
+    indicator[0].style.top = (topIndicator * tablinks[1].offsetHeight) + "px";
+    indicator[0].style.height = tablinks[0].offsetHeight + "px";
+  }

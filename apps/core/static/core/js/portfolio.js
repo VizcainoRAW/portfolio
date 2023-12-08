1601ccbb9600c706e90ff299493124d431f16757
ivan vizcaino <ivanandres1305@gmail.com>
2023-12-08 13:32:51 -0500

    Fixed portfolio.js funtions

--- apps/core/static/core/js/portfolio.js
+++ apps/core/static/core/js/portfolio.js
@@ -1,19 +1,18 @@
-window.addEventListener('scroll', function() {
+document.addEventListener('DOMContentLoaded', function() {
+
+  window.addEventListener('scroll', function() {
     var navbar = document.querySelector('.nav-wrapper');
     var scrollPosition = window.scrollY;
-  
+    
     if (scrollPosition > 0) {
       navbar.classList.add('scrolled');
     } else {
       navbar.classList.remove('scrolled');
     }
+
+  openJob(event, 1)
   });
-
-  function toggleMenu() {
-    var navWrapper = document.querySelector('.nav-wrapper');
-    navWrapper.classList.toggle('active');
-  }
-
+    
   document.getElementById("copyButton").addEventListener("click", function() {
     var mail = "ivanandres1305@gmail.com";
     navigator.clipboard.writeText(mail)
@@ -25,36 +24,29 @@ window.addEventListener('scroll', function() {
     });
     this.innerText = "¡Mail Copied!";
   });
+  
+});
 
-  document.addEventListener('DOMContentLoaded', function() {
-    openJob(event, 'firtsJob');
-  });
+function toggleMenu() {
+  var navWrapper = document.querySelector('.nav-wrapper');
+  navWrapper.classList.toggle('active');
+}
 
-  function openJob(evt, jobId) {
-    var i, tabcontent, tablinks;
-    var topIndicator=0;
+function openJob(evt, jobId) {
+  var i, tabcontent, tablinks;
+  var topIndicator=0;
   
-    tabcontent = document.getElementsByClassName("job");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-  
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-  
-    document.getElementById(jobId).style.display = "block";
-    evt.currentTarget.className += " active";
-  
-    for (i = 0; i < tablinks.length; i++) {
-      if (tabcontent[i].id == jobId) {
-        break;
-      }
-      topIndicator++;
-    }
-  
-    var indicator = document.getElementsByClassName("MuiTabs-indicator");
-    indicator[0].style.top = (topIndicator * tablinks[1].offsetHeight) + "px";
-    indicator[0].style.height = tablinks[0].offsetHeight + "px";
+  tabcontent = document.getElementsByClassName("job");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
+  
+  tablinks = document.getElementsByClassName("tablink");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+
+  document.getElementById(jobId).style.display = "block";
+  evt.currentTarget.className += " active";
+  
+}
